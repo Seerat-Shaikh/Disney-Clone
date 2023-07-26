fc0b93e2373fa6cbd68117c8a246f1cb6c5f4865
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-07-27 00:32:18 +0500

    Details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Home />
+    <div className="App">
+
+      {/* header will not change other component will change */}
+      {/* anything inside router will always be visible */}
+
+      <Router>
+        <Header />
+        <Routes>
+
+          {/* ANything inside routes/switch can be changed */}
+          
+          <Route path="/detail">
+            <Route path='/detail' element={<Detail />} />
+          </Route>
+          <Route path="/">
+            <Route path='/' element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
-   
   );
 }
 
