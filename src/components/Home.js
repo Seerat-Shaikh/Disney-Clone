96d0db9c6ccc43d43c444277f78c6b8d3d662f2c
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-08-14 01:15:26 +0500

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,22 @@ import ImgSlider from './ImgSlider';
 import Viewers from './Viewers';
 import Movies from './Movies';
 import db from '../firebase';
-
-
+import { onSnapshot, collection, query } from "firebase/firestore";
+import { useDispatch } from 'react-redux';
+import { setMovies } from '../features/movie/movieSlice';
 
 function Home() {
+  const dispatch = useDispatch(); //for calling actions to do task
  
   // It will run whenever you will reload page or upload something
   useEffect(()=>{
-    db.collection("movies").onSnapshot((snapshot)=>{
-      console.log(snapshot);
-    })
+    const q = query(collection(db, "movies"))
+    onSnapshot(q, (querySnapshot) => {
+      let tempMovies = querySnapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() }   //unpacking object and putting it in another object returning
+      })
+      dispatch(setMovies(tempMovies))
+    });
   }, [])
 
   return (
